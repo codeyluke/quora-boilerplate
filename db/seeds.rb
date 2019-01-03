b38ba86fbb9dc3d434afda861c2fb0d651bfa037
luke <lukekennedy212@gmail.com>
2019-01-03 14:50:18 +0800

    adding the answer below the question

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-User.create(name: "Malcolm Mcgregor", email: "mcgregor@gmail.com", password: "password123")
-User.create(name: "Matthew Jack", email: "mattrodam@gmail.com", password: "password123")
+User.create(name: "James Mcgregor", email: "james@gmail.com", password: "password123")
+User.create(name: "Justin TimGregor", email: "justin@gmail.com", password: "password123")
 User.create(name:"Jack Lee", email: "jack@gmail.com", password: "password123")
 
 Question.create(question: "What is life?", user_id: 1)
@@ -7,6 +7,6 @@ Question.create(question: "What is Ruby?", user_id: 1)
 Question.create(question: "What is JavaScript?", user_id: 2)
 Question.create(question: "What is Python?", user_id: 3)
 
-
+Answer.create(answer: "Pyhthon is a snake", question_id: 4, user_id: 1)
 Answer.create(answer: "Life is good", question_id: 1, user_id: 2)
 Answer.create(answer: "Ruby is Rails", question_id: 2, user_id: 3)

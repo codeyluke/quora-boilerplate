427338a6be6506451cf3088e8867accc454a649a
luke <lukekennedy212@gmail.com>
2019-01-03 11:29:02 +0800

    added the delete

--- controllers/question.rb
+++ controllers/question.rb
@@ -6,6 +6,7 @@ post '/add_question' do
     question.errors
     redirect to("/users/#{current_user.id}")
 end
+
 get '/question/:id/edit' do
     @question = Question.find(params[:id])
     if current_user && @question.user == current_user
@@ -15,10 +16,29 @@ get '/question/:id/edit' do
     end
 end
 
-post '/update_question/:id' do 
+get '/question/:id/delete' do
+    @question = Question.find(params[:id])
+    if current_user && @question.user == current_user
+        erb :"delete_question"
+    else
+        redirect '/'
+    end
+end
+
+post '/edit_question/:id' do 
     if current_user
         user_question = Question.find(params[:id])
         user_question.update(question: params[:question])
+        redirect to("/users/#{current_user.id}")
+    end
+end
+
+
+delete '/delete_question/:id' do 
+    p "something"
+    if current_user
+        user_question = Question.find(params[:id])
+        user_question.destroy
         redirect to("/users/#{current_user.id}")
     end
 end

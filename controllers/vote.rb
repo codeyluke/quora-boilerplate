bd027790f2f0dd64959d40496ea525b616dd8547
luke <lukekennedy212@gmail.com>
2019-01-04 17:29:14 +0800

    added the remove feature but issue

--- controllers/vote.rb
+++ controllers/vote.rb
@@ -2,3 +2,9 @@ post '/vote/:id/add' do
     Vote.create(answer_id: params[:id], user_id: current_user.id)
     redirect to("/users/#{current_user.id}")
 end 
+
+delete '/vote/:id/delete' do 
+    user_vote = Vote.find_by(answer_id: params[:id], user_id: current_user.id)
+    user_vote.destroy
+    redirect to("/users/#{current_user.id}")
+end

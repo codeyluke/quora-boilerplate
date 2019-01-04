de876177feab57ec80c155196db8331311a4bf64
luke <lukekennedy212@gmail.com>
2019-01-04 10:25:35 +0800

    added the edit and delete answers

--- controllers/answer.rb
+++ controllers/answer.rb
@@ -7,4 +7,26 @@ post '/question/:id/answers/add' do
     redirect to("/users/#{current_user.id}")
 end
 
+get '/answer/:id/edit' do 
+    if current_user
+        @answer = Answer.find(params[:id])
+        erb :"/answer/edit_answer"
+    end
+end
 
+get '/answer/:id/delete' do 
+    @answer = Answer.find(params[:id])
+    erb :"/answer/delete_answer"
+end
+
+patch '/edit_answer/:id' do
+    user_answer = Answer.find(params[:id])
+    user_answer.update(answer: params[:answer])
+    redirect to("/users/#{current_user.id}")
+end
+
+delete '/delete_answer/:id' do 
+    user_answer = Answer.find(params[:id])
+    user_answer.destroy
+    redirect to("/users/#{current_user.id}")
+end
